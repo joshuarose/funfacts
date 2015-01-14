66ebab80638da8ddee4f255c56f4d6f2139e49ed
Joshua Rose <joshuarose@outlook.com>
2015-01-14 12:58:25 -0500

    Finished android tutorial

---
+++ app/src/main/java/com/pitbullprogrammer/funfacts/FunFactsActivity.java
@@ -0,0 +1,50 @@
+package com.pitbullprogrammer.funfacts;
+
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+
+public class FunFactsActivity extends ActionBarActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Declare our View variables and assign them the Views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        final RelativeLayout container = (RelativeLayout) findViewById(R.id.container);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                factLabel.setText(fact);
+                int color = mColorWheel.getColor();
+                container.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+        //Toast.makeText(this, "Yay! Our Activity was created!", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "We're logging from the onCreate() method");
+    }
+}
